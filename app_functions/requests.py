cf26327eadc85811897015736b9d9261c7e6c005
Owen Wurst <owenwurst82@gmail.com>
2024-07-10 22:00:49 -0400

    get listed bookies

--- app_functions/requests.py
+++ app_functions/requests.py
@@ -13,3 +13,27 @@ def get_sports_request(api_token):
         sports.add(sport["key"])
 
     return sports
+
+def get_bookmakers_request(api_token, sport, regions):
+    url = "https://api.the-odds-api.com/v4/sports/{sport}/odds"
+    params = {
+        "api_key": api_token,
+        "regions": regions,
+        "oddsFormat": "american"
+    }
+
+    try:
+        response = requests.get(url.format(sport=sport), params=params)
+
+        bookies = set()
+
+        for data in response.json():
+            bookmaker_data = data["bookmakers"]
+            for bookie in bookmaker_data:
+                bookies.add(bookie["key"])
+
+        return bookies
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return set()
--- build_txts.py
+++ build_txts.py
@@ -6,9 +6,44 @@ if __name__ == "__main__":
 
     sports = req.get_sports_request(api_token)
 
-    # Write sports to file
+    # try to read sports.txt
+    try:
+        with open("sports.txt", "r") as f:
+            old_sports = set(f.read().splitlines())
+    except FileNotFoundError:
+        old_sports = set()
+
+    # combine old and new sports
+    sports = old_sports.union(sports)
+
+    # write sports to sports.txt
     with open("sports.txt", "w") as f:
         for sport in sports:
-            f.write(f"{sport}\n")
+            f.write(sport + "\n")
     
     print("Sports written to sports.txt")
+
+    # get list of regions
+    regions = input("Enter regions separated by commas: ")
+    regions = [region.strip() for region in regions.split(",")]
+
+    # get bookmakers
+    for sport in sports:
+        bookmakers = req.get_bookmakers_request(api_token, sport, regions)
+
+        # try to read bookies.txt
+        try:
+            with open("bookies.txt", "r") as f:
+                old_bookmakers = set(f.read().splitlines())
+        except FileNotFoundError:
+            old_bookmakers = set()
+
+        # combine old and new bookmakers
+        bookmakers = old_bookmakers.union(bookmakers)
+
+        # write bookmakers to bookmakers.txt
+        with open("bookies.txt", "w") as f:
+            for bookmaker in bookmakers:
+                f.write(bookmaker + "\n")
+        
+        print(f"Bookmakers for {sport} written to bookies.txt")
